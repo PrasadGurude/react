861cea53f18e5f38e2f4f78fb493ad3f8014362f
PrasadGurude <prasadgurude76@gmail.com>
2024-01-31 10:06:35 +0530

    cng

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [length,setLength]=useState(length)
 
   return (
-    <>
-      <div className='text-white'>test</div>
-    </>
+    <h1>this is password project</h1>
   )
 }
 
