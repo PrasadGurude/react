9df182704fc572fe67d440a8ceb51668a0164d7d
PrasadGurude <prasadgurude76@gmail.com>
2024-01-31 08:36:44 +0530

    changes 3

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,40 +1,14 @@
-import { useState, useCallback } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed,setNumberAllowed]=useState(false)
-  const [charAllowed,setCharAllowed]=useState(false)
-  const [password, setPassword]=useState(password)
-
-
-const passwordGenerator=useCallback(()=>{
-  let pass="";
-  let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-  if (numberAllowed)  str+="0123456789"
-  if (charAllowed)  str+="!@#$%^&*()_+-={}[]~`"
-
-  for (let i = 0; i <= array.length; i++) {
-    let char =Math.floor( Math.random()*str.length + 1)
-
-    pass= str.charAt(char)
-    
-  }
-
-  setPassword(pass)
-    
-  
-
-},[length, numberAllowed,charAllowed, setPassword])
-
-
-
+  const [length,setLength]=useState(length)
 
   return (
     <>
-       <h1 className='text-white'>thisjahlhgiurgihreiuhghrhguherg</h1>
-
+      <div className='text-white'>test</div>
     </>
   )
 }
