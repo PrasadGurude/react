3c9da88ae7385a74949a041abe18ba499b946bf4
PrasadGurude <prasadgurude76@gmail.com>
2024-02-11 13:48:47 +0530

    change

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-     <h1 className="bg-green-700 p-4">React router</h1>
+      <div className='bg-green-700 p-4'>this is router</div>
     </>
   )
 }
